7abb74b117a6b36d558247f58f1d766afdfbf18e
Arthur Guimarães <arthurdonds@gmail.com>
2022-01-03 19:10:50 -0300

    Criação do componente de tasks e task, introdução da biblioteca react-icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { useState } from "react";
+
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: "Teste 1",
+            day: "15 de janeiro",
+            reminder: false,
+        },
+        {
+            id: 2,
+            text: "Teste 2",
+            day: "12 de janeiro",
+            reminder: true,
+        },
+        {
+            id: 3,
+            text: "Teste 3",
+            day: "11 de janeiro",
+            reminder: false,
+        },
+    ]);
     return (
         <div className="container">
             <Header title="Task tracker" />
+            <Tasks tasks={tasks}/>
         </div>
     );
 }
